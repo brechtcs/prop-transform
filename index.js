185e432bb4eeb4bdfd17d772ec16f4441dc82eb7
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-04-08 18:21:05 +0100

    Respect `Array.prototype.map` immutability

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var clone = require('clone')
 var prop = require('dot-prop')
 
 module.exports = function (path, fn) {
   return function (obj, i, arr) {
-    var val = prop.get(obj, path)
-    prop.set(obj, path, fn(val, i, arr))
-    return obj
+    var object = clone(obj)
+    var val = prop.get(object, path)
+    prop.set(object, path, fn(val, i, arr))
+    return object
   }
 }
