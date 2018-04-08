84720203842c6d63d846222e2e79a53d5f9f7c79
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-04-08 18:08:49 +0100

    Return object properly

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = function (path, fn) {
   return function (obj, i, arr) {
     var val = prop.get(obj, path)
     prop.set(obj, path, fn(val, i, arr))
+    return obj
   }
 }
--- test.js
+++ test.js
@@ -2,6 +2,7 @@ var test = require('tape')
 var transform = require('./')
 
 test('Map array of objects', function (t) {
+  var output
   var arr = [{
     name: 'First',
     data: {
@@ -14,12 +15,13 @@ test('Map array of objects', function (t) {
     }
   }]
 
-  arr.map(transform('name', (name, i) => (i + 1) + '. ' + name))
-  arr.map(transform('data.some', str => str.toUpperCase()))
+  output = arr
+    .map(transform('name', (name, i) => (i + 1) + '. ' + name))
+    .map(transform('data.some', str => str.toUpperCase()))
 
-  t.equals(arr[0].name, '1. First', 'first name transform')
-  t.equals(arr[0].data.some, 'STUFF', 'first nested data transform')
-  t.equals(arr[1].name, '2. Second', 'second name transform')
-  t.equals(arr[1].data.some, 'OTHER STUFF', 'second nested data transform')
+  t.equals(output[0].name, '1. First', 'first name transform')
+  t.equals(output[0].data.some, 'STUFF', 'first nested data transform')
+  t.equals(output[1].name, '2. Second', 'second name transform')
+  t.equals(output[1].data.some, 'OTHER STUFF', 'second nested data transform')
   t.end()
 })
