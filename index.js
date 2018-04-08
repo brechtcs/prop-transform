c3c55737f24f8a81cf16479f93210ab0a812d9fb
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-04-08 12:56:10 +0100

    Remove conditional

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var prop = require('dot-prop')
 module.exports = function (path, fn) {
   return function (obj) {
     var val = prop.get(obj, path)
-    if (val) {
-      prop.set(obj, path, fn(val))
-    }
+    prop.set(obj, path, fn(val))
   }
 }
