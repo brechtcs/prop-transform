4be04c6323a64f0596d21d56932e3ef054572c9d
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2018-04-12 07:30:45 +0200

    Only clone transformed property

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var prop = require('dot-prop')
 
 module.exports = function (path, fn) {
   return function (obj, i, arr) {
-    var object = clone(obj)
-    var val = prop.get(object, path)
-    prop.set(object, path, fn(val, i, arr))
+    var object = Object.assign({}, obj)
+    var val = prop.get(obj, path)
+    prop.set(object, path, fn(clone(val), i, arr))
     return object
   }
 }
